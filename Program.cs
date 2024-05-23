6a9f3a65a382657d2aba1758c47e6ab601dbd597
joshu <joshu@REEDPC>
2024-05-23 15:04:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Challenge3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string containing both letters and numbers. I will return the sum of the numbers");
+           string myString = Console.ReadLine();
+
+            double sum = 0;
+            for (int i = 0;i<myString.Length;i++)
+            {
+                double number = Char.GetNumericValue(myString, i);
+                if (number != -1)
+                {
+                    sum = sum + number;
+                }
+             
+            }
+            Console.WriteLine($"The total of the numeric digits is {sum}");
+        }
+    }
+}
